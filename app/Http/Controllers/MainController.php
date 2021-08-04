68f77176c950f698af998bdc1bd8e2fd5573c47e
MishaR <mesharoman12@gmail.com>
2021-08-04 23:19:17 +0300

    Добавлен главный контроллер, и добавлен роутинг

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home() {
+        return view('home');
+    }
+
+    public function categories() {
+        return view('categories');
+    }
+
+    public function product($product = null) {
+        return view('product', ['product' => $product]);
+    }
+
+    public function category($category) {
+        return view('category', compact('category'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',  [MainController::class, 'home']);
+
+Route::get('/categories', [MainController::class, 'categories']);
+
+Route::get('/{category}', [MainController::class, 'category']);
+
+Route::get('/mobiles/{param?}', [MainController::class, 'product']);
+
+
+
