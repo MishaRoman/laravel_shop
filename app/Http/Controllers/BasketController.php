e609230d617d65f2ff6834314671de366dab5630
MishaR <mesharoman12@gmail.com>
2021-09-13 21:49:39 +0300

    Большинство логики из BasketController перенесено в отдельный класс

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -5,33 +5,29 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Classes\Basket;
 
 class BasketController extends Controller
 {
     public function basket()
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
+        $order = (new Basket())->getOrder();
+        
 
         return view('basket', compact('order'));
     }
 
     public function basketPlace()
     {
-        $orderId = session('orderId');
-        $order = Order::findOrFail($orderId);
+        $order = (new Basket())->getOrder();
 
         return view('order', compact('order'));
     }
 
     public function basketConfirm(Request $request)
     {
-        $orderId = session('orderId');
 
-        $order = Order::findOrFail($orderId);
-        
-        $success = $order->saveOrder($request->name, $request->phone);
+        $success = (new Basket())->saveOrder($request->name, $request->phone);
 
         if ($success) {
             session()->flash('success', 'Ваш заказ принят на обработку');
@@ -46,28 +42,7 @@ class BasketController extends Controller
 
     public function basketAdd(Product $product)
     {
-        $orderId = session('orderId');
-        if ( is_null($orderId) ) {
-            $order = Order::create();
-            session(['orderId' => $order->id]);
-        } else {
-            $order = Order::findOrFail($orderId);
-        }
-
-        if($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            $pivotRow->count++;
-            $pivotRow->update();
-        } else {
-            $order->products()->attach($product->id);
-        }
-
-        if (Auth::check()) {
-            $order->user_id = Auth::id();
-            $order->save();
-        }
-
-        Order::changeFullPrice($product->price);
+        (new Basket(true))->addProduct($product);
 
         session()->flash('success', 'Добавлен товар ' . $product->name);
 
@@ -76,23 +51,9 @@ class BasketController extends Controller
 
     public function basketRemove(Product $product)
     {
-        $orderId = session('orderId');
+        (new Basket())->removeProduct($product);
 
-        $order = Order::findOrFail($orderId);
-
-        if ($order->products->contains($product->id)) {
-            $pivotRow = $order->products()->where('product_id', $product->id)->first()->pivot;
-            if ($pivotRow->count < 2) {
-                $order->products()->detach($product->id);
-            } else {
-                $pivotRow->count--;
-                $pivotRow->update();
-            }
-        }
-
-        Order::changeFullPrice(-$product->price);
-
-        session()->flash('warning', 'Удален товар' . $product->name);
+        session()->flash('warning', 'Удален товар ' . $product->name);
 
         return redirect()->route('basket');
     }
